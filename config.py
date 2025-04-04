3629250bc4f4d051c803ea6df34e538c8ec3da80
𝐓𝐄𝐀𝐌 𝐒𝐀𝐍𝐊𝐈 <teamsanki001@gmail.com>
2025-04-04 22:18:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -69,8 +69,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Teamsankinetworkk")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Teamsankinetworkk")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SANKINETWORK")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/SANKINETWORK")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
