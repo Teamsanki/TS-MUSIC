49240d3756586d640510e721dc2442be1d0646b0
𝐓𝐄𝐀𝐌 𝐒𝐀𝐍𝐊𝐈 <teamsanki001@gmail.com>
2026-04-03 11:35:56 +0530

    Update Youtube.py

--- SANKIXD/platforms/Youtube.py
+++ SANKIXD/platforms/Youtube.py
@@ -7,9 +7,9 @@ import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from py_yt import VideosSearch
-from Ayush.utils.formatters import time_to_seconds
+from SANKIXD.utils.formatters import time_to_seconds
 import aiohttp
-from Ayush import LOGGER
+from SANKIXD import LOGGER
 from typing import Union
 
 YOUR_API_URL = None
@@ -17,7 +17,7 @@ FALLBACK_API_URL = "https://shrutibots.site"
 
 async def load_api_url():
     global YOUR_API_URL
-    logger = LOGGER("Ayush.platforms.Youtube.py")
+    logger = LOGGER("SANKIXD.platforms.Youtube.py")
     
     try:
         async with aiohttp.ClientSession() as session:
